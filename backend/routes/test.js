8db57442a157acbce01600cd328d012a9330ae93
martinyounghoonkim <hoons0131@gmail.com>
2018-07-03 00:27:40 +0900

    Edit socket io directory

--- backend/routes/test.js
+++ backend/routes/test.js
@@ -2,10 +2,20 @@ const express = require('express');
 const router = express.Router();
 const io = require('../core/socket');
 
-router.get('/', () => {
-    io.emit('add user', {
-        key: 'foo'
-    });
-});
 
-module.exports = router;
+
+exports.testing = () => {
+    io.on('connection', (socket) => {
+        var addedUser = false;
+        // when the client emits 'add user', this listens and executes
+        socket.on('add user', (username) => {
+            if (addedUser) return;
+            console.log(username);
+            // we store the username in the socket session for this client
+            socket.username = username;
+            addedUser = true;
+        });
+    });
+}
+
+
